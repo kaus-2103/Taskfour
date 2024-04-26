86c03cecf9a5f98e6ff0a04031daa225a4bcbeba
Kaushik Roy <63772738+kaus-2103@users.noreply.github.com>
2024-04-26 15:09:22 +0600

    Update sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,27 +2,28 @@ class SessionsController < ApplicationController
     def new
     end
   
-    def create
-      user = User.find_by(email: params[:email])
-      
-      if user
-        if user.authenticate(params[:password]) && !user.authentication 
-          user.update_last_login_time
-          redirect_to root_path, alert: "You are not authorized to access this page."
-        elsif user.status == "Blocked"
-          redirect_to signin_path, notice:"You have been Blocked"
-        elsif user.authenticate(params[:password]) && user.authentication == true
-          session[:user_id] = user.id
-          session[:current_user_id] = user.id 
-          user.update_last_login_time
-          redirect_to menu_path(current_user_id: user.id), notice: "Logged in successfully!" 
-        else
-          redirect_to signin_path, notice:"Invalid email or password"
-        end
-      else
-        redirect_to signin_path, notice:"Invalid email or password"
-      end
+   def create
+  user = User.find_by(email: params[:email])
+  
+  if user
+    if user.status == "Blocked"
+      redirect_to signin_path, notice: "You have been Blocked"
+    elsif user.authenticate(params[:password]) && !user.authentication 
+      user.update_last_login_time
+      redirect_to root_path, alert: "You are not authorized to access this page."
+    elsif user.authenticate(params[:password]) && user.authentication == true
+      session[:user_id] = user.id
+      session[:current_user_id] = user.id 
+      user.update_last_login_time
+      redirect_to menu_path(current_user_id: user.id), notice: "Logged in successfully!" 
+    else
+      redirect_to signin_path, notice: "Invalid email or password"
     end
+  else
+    redirect_to signin_path, notice: "Invalid email or password"
+  end
+end
+
     
     
   
